aa5d2bc22e92faedffc8e571040359500a54b74f
Ishimaru17 <ishimaru.campora@gmail.com>
2019-04-01 10:52:35 +0100

    __init__.py initialization

--- __init__.py
+++ __init__.py
@@ -1,15 +1,23 @@
-from mycroft import MycroftSkill, intent_file_handler
-
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
 
 class CampoTest(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
-
-    @intent_file_handler('test.campo.intent')
-    def handle_test_campo(self, message):
-        self.speak_dialog('test.campo')
 
 
-def create_skill():
-    return CampoTest()
+	def __init__(self):
+
+	#that all the requirement for the function
+	#the requirement are the .voc documents needed
+	#if something is optional, there is the .optional() option
+	@intent_handler(IntentBuilder("").require("test.campo").build())
+	def initialize(self, message):
+		#get the message which was says by the user
+		utterance = message.data.get['utterance']
+		#Mycroft will say the content of the error.dialog file.
+		self.speak_dialog('test.campo')
+		#Mycroft will repeat exactly what was said.
+		self.speak_dialog(utterance)
+
+def create_skills():
+	return CampoTest()
 
