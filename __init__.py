44e071a9cbd05297505a1b3b15d498d2f05c36a4
Ishimaru17 <ishimaru.campora@gmail.com>
2019-04-01 14:57:47 +0100

    try a new initialization

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_handler
+from mycroft.skills.core import MycroftSkill, intent_handler
 
 class CampoTest(MycroftSkill):
 
-
+	def __init__(self):
+		super(CampoTest, self).__init__(name="CampoTest")
 	#that all the requirement for the function
 	#the requirement are the .voc documents needed
 	#if something is optional, there is the .optional() option
