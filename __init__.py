d9746c4bee3477b7e41a4e297876535b831a5e71
Ishimaru17 <ishimaru.campora@gmail.com>
2019-04-01 16:11:26 +0100

    try of intent

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 
-class CampoTest(MycroftSkill):
+class CampoSkill(MycroftSkill):
 
 	def __init__(self):
-		super(CampoTest, self).__init__(name="CampoTest")
+		super(CampoSkill, self).__init__(name="CampoSkill")
 	#that all the requirement for the function
 	#the requirement are the .voc documents needed
 	#if something is optional, there is the .optional() option
-	@intent_handler(IntentBuilder("").require("campo"))
-	def initialize(self, message):
+	@intent_handler(IntentBuilder("CampoIntent").require("campo").build())
+	def handler_campo__intent(self, message):
 		#get the message which was says by the user
 		utterance = message.data.get['utterance']
 		#Mycroft will say the content of the error.dialog file.
@@ -20,5 +20,5 @@ class CampoTest(MycroftSkill):
 		pass
 
 def create_skill():
-	return CampoTest()
+	return CampoSkill()
 
