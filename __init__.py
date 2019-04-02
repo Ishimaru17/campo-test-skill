fe18628b469a70bc54652f74aa24a9518e899b2d
Ishimaru17 <ishimaru.campora@gmail.com>
2019-04-02 09:40:18 +0100

    repeat the utterance

--- __init__.py
+++ __init__.py
@@ -1,24 +1,33 @@
+import re
+
+#Packages needed to use Microft
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 
+#The class must be the inheritance of the mycroftSkill class
 class CampoSkill(MycroftSkill):
 
+	#Initialization thank to the __init__ method of the superclass, with the name of our skill
 	def __init__(self):
 		super(CampoSkill, self).__init__(name="CampoSkill")
+
 	#that all the requirement for the function
 	#the requirement are the .voc documents needed
 	#if something is optional, there is the .optional() option
+	#the intent builder arg match the name of the func below 
 	@intent_handler(IntentBuilder("CampoIntent").require("campo").build())
 	def handler_campo__intent(self, message):
 		#get the message which was says by the user
 		utterance = message.data.get('utterance')
-		#Mycroft will say the content of the error.dialog file.
-		#Mycroft will repeat exactly what was said.
-		self.speak_dialog(utterance)
+		#remove the occurance of the campo word in the message
+		repeat = re.sub('^.*?' + message.data['campo'], '', utterance)
+		self.speak_dialog(repeat.strip())
 
+	#stop function
 	def stop(self):
 		pass
 
+#Creation of the skill.
 def create_skill():
 	return CampoSkill()
 
